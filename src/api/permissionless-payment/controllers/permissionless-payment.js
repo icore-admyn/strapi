10ad4e5c9ef0f9eeb89233e7567f181bf09f972d
ryansallen98 <115708378+ryansallen98@users.noreply.github.com>
2023-08-27 18:49:25 +0100

    Initial Commit

---
+++ src/api/permissionless-payment/controllers/permissionless-payment.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * permissionless-payment controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::permissionless-payment.permissionless-payment');
---
+++ src/extensions/menus/strapi-server.js
@@ -0,0 +1,26 @@
+// ./src/extensions/menus/strapi-server.js`
+'use strict';
+
+module.exports = plugin => {
+    // Get current `MenuItem` attributes.
+    const defaultAttrs = plugin.contentTypes['menu-item'].schema.attributes;
+
+    // Define custom attributes for `MenuItem` the same way they would be defined
+    // on any other schema.
+    const customAttrs = {
+        isButton: {
+            type: 'boolean',
+        },
+        parentMenu: {
+            type: 'string',
+        },
+    };
+
+    // Extend the `MenuItem` content type with custom attributes.
+    plugin.contentTypes['menu-item'].schema.attributes = {
+        ...defaultAttrs,
+        ...customAttrs,
+    };
+
+    return plugin;
+};
