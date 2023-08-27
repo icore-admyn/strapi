cfd819d9eb58d207d80d1f6183cd909fbb9ff7f5
ryansallen98 <115708378+ryansallen98@users.noreply.github.com>
2023-08-27 22:15:14 +0100

    Initial Commit

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('HOST', 'admin.icorepay.io'),
+  url: env('URL', 'admin.icorepay.io'),
   app: {
     keys: env.array('APP_KEYS'),
   },
