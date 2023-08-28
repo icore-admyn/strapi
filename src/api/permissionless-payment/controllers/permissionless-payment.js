b8b69e525c495554c13011033625719b887b8d66
ryansallen98 <115708378+ryansallen98@users.noreply.github.com>
2023-08-28 11:35:15 +0100

    Uodate content types

--- src/api/permissionless-payment/controllers/permissionless-payment.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * permissionless-payment controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::permissionless-payment.permissionless-payment');
--- src/api/permissionless-payment/routes/permissionless-payment.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * permissionless-payment router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::permissionless-payment.permissionless-payment');
