c65ce63eef862d2f159f15afffa023adb4376088
ryansallen98 <115708378+ryansallen98@users.noreply.github.com>
2023-08-29 12:49:04 +0100

    Update content types

---
+++ src/api/permissionless-payment/routes/permissionless-payment.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * permissionless-payment router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::permissionless-payment.permissionless-payment');
